3df81b7414dfadb094035c1e4b848a5c5b513d2c
BakinwaStephen <bakinwastephen.edu@gmail.com>
2019-03-10 21:51:12 +0100

    We made some adjustment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('title')->nullable();
+            $table->string('fullname')->nullable();
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('othernames')->nullable();
+            $table->string('username')->nullable();
+            $table->text('desc')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('phone2')->nullable();
+            $table->string('role')->nullable();
+            $table->string('other_role')->nullable();
+            $table->string('status')->nullable();
+            $table->integer('age')->default(0);
+            $table->string('gender')->nullable();
+            $table->string('birthdate')->nullable();
+            $table->string('home_address')->nullable();
+            $table->string('img_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
